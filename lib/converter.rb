d8f6c4d957b603a7a37bf969314fcbc8b3bf7a3b
Cleiton Francisco <cleitonfco@gmail.com>
2009-08-18 20:11:09 -0300

    quinta dupla

--- lib/converter.rb
+++ lib/converter.rb
@@ -2,10 +2,20 @@ class Converter
 
   def initialize(valor, moeda)
     @valor = valor
+    @moeda = moeda
   end
   
   def to_dolar
     1.86 * @valor
+  end
+  
+  def to_euro
+    case @moeda
+      when "BRL"
+        3.40 * @valor
+      when "USD"
+        1.96 * @valor
+    end
   end
 
 end
--- spec/converter_spec.rb
+++ spec/converter_spec.rb
@@ -4,10 +4,6 @@ describe 'converter' do
   it "deve existir" do
      Converter
   end
-  it "deve ter um valor do tipo valido" do
-    pending
-    Converter.to_dolar("5.df").should_not be_valid
-  end
   
   it "deve transformar 1 real em dolares" do
     a = Converter.new(1, "BRL")
@@ -19,4 +15,19 @@ describe 'converter' do
     b.to_dolar.should == 18.60
   end
   
+  it "deve transformar 1 real em euros" do
+    a = Converter.new(1, "BRL")
+    a.to_euro.should == 3.40
+  end
+  
+  it "deve transformar 10 reais em euros" do
+    a = Converter.new(10, "BRL")
+    a.to_euro.should == 34.00
+  end
+  
+  it "deve transformar 1 dolar em euro" do
+    a = Converter.new(1, "USD")
+    a.to_euro.should == 1.96
+  end
+
 end
